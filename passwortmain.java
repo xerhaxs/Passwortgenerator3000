aac0b2861584b9ea7c09a5e036f366470f675947
xerhaxs <xerhaxs@protonmail.com>
2022-01-31 15:01:58 +0100

    Sum Bildschirm test

--- passwortmain.java
+++ passwortmain.java
@@ -1,6 +1,10 @@
+import sum.kern.Bildschirm;
+
 public class passwortmain {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Bildschirm Bildschirm1;
+        Bildschirm1 = new Bildschirm();
     }
 }
