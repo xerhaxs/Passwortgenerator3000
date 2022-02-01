63f324dcdb9a7baff9d5dad5accfc30a7d86ab6f
DasIstEinName <jonaszwick1@web.de>
2022-02-01 09:15:41 +0100

    Generator für alle zeichen

---
+++ Generator1alle.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import sum.kern.*;
+public class Generator1alle {
+    Random Generator1zahlen;
+    Stift Stift1schreiben;
+    public Generator1alle() {
+        Generator1zahlen = new Random();
+        Stift1schreiben = new Stift();
+    }
+
+    public void Generiere () {
+        int upperbound = 94;
+        int int_random = Generator1zahlen.nextInt(upperbound)+32;
+        Stift1schreiben.bewegeBis(500,600);
+        char value_char = (char) int_random;
+        Stift1schreiben.schreibeText(" "+value_char);
+    }
+}
--- passwortmain.java
+++ passwortmain.java
@@ -1,15 +1,19 @@
-import sum.kern.Bildschirm;
-import sum.kern.Stift;
+import sum.kern.*;
 
 public class passwortmain {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        Bildschirm Bildschirm1;
-        Bildschirm1 = new Bildschirm();
+        Bildschirm Bildschirm1anzeige;
         Stift stift1schreiben;
+        Generator1alle generator1generieren;
+
+        Bildschirm1anzeige = new Bildschirm();
         stift1schreiben = new Stift();
+        generator1generieren = new Generator1alle();
+
+        generator1generieren.Generiere();
         stift1schreiben.bewegeBis(500, 500);
         stift1schreiben.schreibeText("Hallo Welt");
+
     }
 }
