376017bd6377a6c63cee3f04084ed25764e1b801
xerhaxs <xerhaxs@protonmail.com>
2022-02-01 16:03:35 +0100

    gui anfang

--- classes/gui.java
+++ classes/gui.java
@@ -1,4 +1,17 @@
 package classes;
+import javax.swing.*;
+import java.awt.*;
 
-public class gui {
+class gui{
+    public static void main(String args[]){
+        JFrame frame = new JFrame("Passwortgenerator3000");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JCheckBox checkbox1 = new JCheckBox("Buchstaben");
+        JButton buttongen1 = new JButton("Passwort generieren");
+        frame.getContentPane().add(checkbox1);
+        checkbox1.
+        frame.getContentPane().add(buttongen1);
+        frame.setVisible(true);
+    }
 }
