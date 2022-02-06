bf34e0bbd246e649f30719e2b2a47afd36dff0e7
xerhaxs <xerhaxs@protonmail.com>
2022-02-06 15:53:47 +0100

    Einrücken

--- classes/generator.java
+++ classes/generator.java
@@ -3,50 +3,7 @@ package classes;
 import java.util.Random;
 
 public class generator{
-    // Grossbuchstaben Wert
-    private static boolean zGrossbuchstaben;
-    public static void setGrossbuchstaben(boolean pGrossbuchstaben) {
-        zGrossbuchstaben = pGrossbuchstaben;
-    }
-    public static boolean getzGrossbuchstaben() {
-        return zGrossbuchstaben;
-    }
 
-    // Kleinbuchstaben Wert
-    private static boolean zKleinbuchstaben;
-    public static void setKleinbuchstaben(boolean pKleinbuchstaben) {
-        zKleinbuchstaben = pKleinbuchstaben;
-    }
-    public boolean getzKleinbuchstaben() {
-        return zKleinbuchstaben;
-    }
-
-    // Ziffern Wert
-    private static boolean zZiffern;
-    public static void setZiffern(boolean pZiffern) {
-        zZiffern = pZiffern;
-    }
-    public boolean getzZiffern() {
-        return zZiffern;
-    }
-
-    // Sonderzeichen Wert
-    private static boolean zSonderzeichen;
-    public static void setSonderzeichen(boolean pSonderzeichen) {
-        zSonderzeichen = pSonderzeichen;
-    }
-    public boolean getzSonderzeichen() {
-        return zSonderzeichen;
-    }
-
-    // Passwortlaenge Wert
-    private static int zPasswortlaenge;
-    public static void setPasswortlaenge(int pPasswortlaenge) {
-        zPasswortlaenge = pPasswortlaenge;
-    }
-    public int getzPasswortlaenge() {
-        return zPasswortlaenge;
-    }
 
 
     public static String generierePasswort(int length) {
